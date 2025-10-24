cbe3ae0965a6175becc6738586cc49b91902d040
Maxime.C <macorona.41@gmail.com>
2025-10-24 14:17:35 +0200

    Initial commit

---
+++ PixelArtClass_Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="PixelArtClass_Tileset" tilewidth="32" tileheight="32" tilecount="45" columns="9">
+ <image source="../../../../Downloads/PixelArtClass_Course1_Environment/PixelArtClass_Tileset_NameSurname.png" width="288" height="160"/>
+</tileset>
